815a0141cef7feccf11d455223be0aa67741e800
minazaher <menazaher115@gmail.com>
2023-08-21 19:54:16 +0300

    maps is working

---
+++ app/src/main/java/com/example/notesapp/Activity/GetLocationActivity.java
@@ -0,0 +1,16 @@
+package com.example.notesapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.notesapp.R;
+
+public class GetLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_location);
+    }
+}
