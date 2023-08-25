1ede6fb6427fbf44524e737c6c32623cf20b8045
minazaher <menazaher115@gmail.com>
2023-08-25 05:55:29 +0300

    creating new task

---
+++ app/src/main/java/com/example/notesapp/Activity/CreateTaskActivity.java
@@ -0,0 +1,16 @@
+package com.example.notesapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.notesapp.R;
+
+public class CreateTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_task);
+    }
+}
---
+++ app/src/main/java/com/example/notesapp/Model/Task.java
@@ -0,0 +1,4 @@
+package com.example.notesapp.Model;
+
+public class Task {
+}
