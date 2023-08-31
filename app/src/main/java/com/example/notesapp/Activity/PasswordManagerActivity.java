121bfbeaad264461f085ef036161329f45eec63c
minazaher <menazaher115@gmail.com>
2023-08-31 16:03:01 +0300

    password manager is working

---
+++ app/src/main/java/com/example/notesapp/Activity/PasswordManagerActivity.java
@@ -0,0 +1,16 @@
+package com.example.notesapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.notesapp.R;
+
+public class PasswordManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_manager);
+    }
+}
